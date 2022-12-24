403a1019968a5ad3158b5c6e757f6e907deb04bc
MikeRT16 <83572288+MikeRT16@users.noreply.github.com>
2022-12-24 11:36:01 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+/* 
+Solo letras minusculas
+No se permite acentuación de palabras 
+
+Solo letras minusculas
+No se permite acentuación de palabras   
+*/
+//----------------------- ENCRIPTAR-----------------------------
+
+var botonEncriptar = document.querySelector("#btn-encriptar");
+botonEncriptar.addEventListener("click", encriptar);
+
+//---------------------- DESENCRIPTAR---------------------------
+
+var botonDesencriptar = document.querySelector("#btn-desencriptar");
+botonDesencriptar.addEventListener("click", desencriptar);
+   
+
+
+
+
